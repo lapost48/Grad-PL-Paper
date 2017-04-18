7e30ca6003519e05ac1fcbc70da298d086adf813
Nick LaPosta <lapost48@gmail.com>
2017-04-17 22:32:10 -0400

    Completed C vector dot product script

---
+++ matmul/dot.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int vectors[argc-1];
+    int sum = 0;
+    for(int i = 1; i <= argc / 2; i++) {
+        printf("%d*%d\n",strtol(argv[i],NULL,10),strtol(argv[(argc / 2) + i],NULL,10));
+        sum += strtol(argv[i],NULL,10) * strtol(argv[(argc / 2) + i],NULL,10);
+    }
+    printf("Dot Product = %d\n", sum);
+    return sum;
+}
