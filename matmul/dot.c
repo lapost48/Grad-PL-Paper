147d67455e17961b337cd05fac17a191dc52a9d8
Nick LaPosta <lapost48@gmail.com>
2017-04-18 00:56:23 -0400

    Fixed C output formatting

--- matmul/dot.c
+++ matmul/dot.c
@@ -5,9 +5,10 @@ int main(int argc, char *argv[]) {
     int vectors[argc-1];
     int sum = 0;
     for(int i = 1; i <= argc / 2; i++) {
-        printf("%d*%d\n",strtol(argv[i],NULL,10),strtol(argv[(argc / 2) + i],NULL,10));
+        //printf("%d*%d\n",strtol(argv[i],NULL,10),strtol(argv[(argc / 2) + i],NULL,10));
         sum += strtol(argv[i],NULL,10) * strtol(argv[(argc / 2) + i],NULL,10);
     }
-    printf("Dot Product = %d\n", sum);
+    //printf("Dot Product = %d\n", sum);
+    printf("%d",sum);
     return sum;
 }
